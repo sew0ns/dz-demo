685808e305fa4852653ef0767cd5ea5c0190e325
Elvin <elvin.dzhabiev@mail.ru>
2026-02-22 14:33:55 +0500

    Add rooms and schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from './schedule/schedule.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
